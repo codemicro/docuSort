f8fb64e330dd7f059ba82a123e4de52aa62a87fa
Tom <tom@tdpain.net>
2020-09-05 21:25:11 +0100

    Finish build file

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -2,25 +2,61 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 
+	"github.com/codemicro/docuSort/internal/storage"
 	"github.com/codemicro/docuSort/internal/templates"
+)
+
+const (
+	version = "1.0.0"
 )
 
 //go:generate go get -u github.com/valyala/quicktemplate/qtc
 //go:generate qtc -dir=../../internal/templates
 
 func main() {
-	fmt.Println("AAAAAA")
+	fmt.Println("docuSort build tool v" + version)
+	fmt.Println()
+
+	documents, err := storage.GetFiles()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	subjects := storage.GetSubjects(documents)
 
 	var buf bytes.Buffer
-	templates.WritePageTemplate(&buf, &templates.MainPage{
-		Subjects: []string{"Computer Science", "Maths", "Physics"},
+	templates.WritePageTemplate(&buf, &templates.HomePage{
+		Subjects: subjects,
 	})
 
-	fmt.Printf("buf=\n%s", buf.Bytes())
-
 	ioutil.WriteFile("index.html", buf.Bytes(), 0644)
+
+	for _, subject := range subjects {
+		buf.Reset()
+		subjectFiles := storage.FilterWhereSubjectIs(subject, documents)
+		jr, err := json.Marshal(subjectFiles)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		templates.WritePageTemplate(&buf, &templates.SubjectPage{
+			Subjects: subjects,
+			Subject:  subject,
+			Files:    string(jr),
+		})
+
+		_ = os.Mkdir(subject, os.ModeDir)
+		ioutil.WriteFile(filepath.Join(subject, "index.html"), buf.Bytes(), 0644)
+	}
 
 }
