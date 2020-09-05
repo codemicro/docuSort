314324e4f3c73db20ea96efdd60e5fe7e3e0fdef
Tom <tom@tdpain.net>
2020-09-05 12:23:50 +0100

    Add initial quicktemplate stuff for build

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/codemicro/docuSort/internal/templates"
+)
+
+//go:generate go get -u github.com/valyala/quicktemplate/qtc
+//go:generate qtc -dir=../../internal/templates
 
 func main() {
 	fmt.Println("AAAAAA")
+
+	var buf bytes.Buffer
+	templates.WritePageTemplate(&buf, &templates.MainPage{
+		Subjects: []string{"Computer Science", "Maths", "Physics"},
+	})
+
+	fmt.Printf("buf=\n%s", buf.Bytes())
+
+	ioutil.WriteFile("index.html", buf.Bytes(), 0644)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/codemicro/docuSort
 
 go 1.14
+
+require github.com/valyala/quicktemplate v1.6.2
