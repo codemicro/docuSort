28942298d08e77e611e27bd01cc5c445a83f97e3
Tom <tom@tdpain.net>
2020-09-11 19:09:45 +0100

    Nuke v1

--- scripts/buildAll.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-go build github.com/codemicro/docuSort/cmd/save
-go build github.com/codemicro/docuSort/cmd/build
--- scripts/buildAll.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-go build github.com/codemicro/docuSort/cmd/save
-go build github.com/codemicro/docuSort/cmd/build
